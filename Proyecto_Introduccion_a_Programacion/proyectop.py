d127df870fcba3e6c129642ac6cc9d9668ab8a10
Daniels2405 <danielbarrientossalas@hotmail.com>
2025-03-06 19:50:22 -0600

    Inicio de sesion terminado y funcional

--- Proyecto_Introduccion_a_Programacion/proyectop.py
+++ Proyecto_Introduccion_a_Programacion/proyectop.py
@@ -3,6 +3,10 @@ userAdmin = "admin"
 passAdmin = "admin"
 user_personal = "personal"
 pass_personal = "personal"
+user = ""
+password = ""
+intentos = 1
+#Librerias
 import sys
 #Programa
 print("Bienvenido al sistema de facturacion de Austin's Electronics")
@@ -11,7 +15,11 @@ password = input("Ingrese contraseña: ")
 
 while user != (userAdmin or user_personal) or password != (passAdmin or pass_personal):
         print("Usuario o contraseña incorrectos")
+        intentos += 1
+        print("Le quedan ", 4 - intentos, " intentos")
         user = input("Ingrese nombre de Usuario: ")
         password = input("Ingrese contraseña: ")
-        if user == "a":
+        if intentos == 3:
+                print("Ha superado el numero de intentos permitidos")
                 sys.exit()
+print("Bienvenido al sistema de facturacion de Austin's Electronics")
