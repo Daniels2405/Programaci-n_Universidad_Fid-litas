31ae559e1afe46a68d7ad84b58ec7814682367e2
Daniels2405 <danielbarrientossalas@hotmail.com>
2025-03-06 21:50:46 -0600

    Registro listo, Menu funcional pero con el ciclo faltante

--- Proyecto_Introduccion_a_Programacion/proyectop.py
+++ Proyecto_Introduccion_a_Programacion/proyectop.py
@@ -6,13 +6,16 @@ pass_personal = "personal"
 user = ""
 password = ""
 intentos = 1
+
 #Librerias
 import sys
+
 #Programa
 print("Bienvenido al sistema de facturacion de Austin's Electronics")
 user = input("Ingrese nombre de Usuario: ")
 password = input("Ingrese contraseña: ")
 
+#Inicio de sesion en caso de que coloque el usuario y contraseña incorrectos
 while user != (userAdmin or user_personal) or password != (passAdmin or pass_personal):
         print("Usuario o contraseña incorrectos")
         intentos += 1
@@ -22,4 +25,7 @@ while user != (userAdmin or user_personal) or password != (passAdmin or pass_per
         if intentos == 3:
                 print("Ha superado el numero de intentos permitidos")
                 sys.exit()
+
+#Inicio de programa
 print("Bienvenido al sistema de facturacion de Austin's Electronics")
+
