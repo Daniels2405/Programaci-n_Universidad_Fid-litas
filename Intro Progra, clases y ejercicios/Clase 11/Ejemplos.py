b0fb7ad21d797ccf1101fd0a738023ec5d769479
Daniels2405 <danielbarrientossalas@hotmail.com>
2025-03-24 20:23:16 -0600

    Fin de clase 11

--- Intro Progra, clases y ejercicios/Clase 11/Ejemplos.py
+++ Intro Progra, clases y ejercicios/Clase 11/Ejemplos.py
@@ -24,3 +24,37 @@ for i in range (5):
         estudiantes[i][j] = int(input("Nota: "))
 print("Las notas de los estudiantes son: ", estudiantes)
 '''
+#------------microbus----------------
+Asientos = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+print("Bienvenido al programa de reservas de microbus")
+i = 0
+j = 0
+rep = 0
+while rep == 0:
+    print ("Ingrese el horario en cual desea reservar")
+    print ("1. 6:00 am")
+    print ("2. 8:00 am")
+    print ("3. 10:00 am")
+    print ("4. 12:00 pm")
+    horario = int(input("Horario: "))
+    if horario == 1:
+        print("Ingrese el asiento que desea reservar de 20 disponibles")
+        Asiento = int(input("Asiento: "))
+        Asientos[0][Asiento - 1] = 1
+    elif horario == 2:
+        print("Ingrese el asiento que desea reservar de 20 disponibles")
+        Asiento = int(input("Asiento: "))
+        Asientos[1][Asiento - 1] = 1
+    elif horario == 3:
+        print("Ingrese el asiento que desea reservar de 20 disponibles")
+        Asiento = int(input("Asiento: "))
+        Asientos[2][Asiento - 1] = 1
+    elif horario == 4:
+        print("Ingrese el asiento que desea reservar de 20 disponibles")
+        Asiento = int(input("Asiento: "))
+        Asientos[3][Asiento -1] = 1
+    print("Desea hacer otra reserva?")
+    print("1. Si")
+    print("2. No")
+    rep = int(input("Respuesta: "))
+print("Las reservas son: ", Asientos)
