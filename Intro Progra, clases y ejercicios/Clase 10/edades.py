beb4faa197dfec445cff9151c32557141b01c858
Daniel Barrientos Salas <danielbarrientossalas@hotmail.com>
2025-03-25 11:26:20 -0600

    Delete Intro Progra, clases y ejercicios directory
    Esta en la carpeta de la materia

--- Intro Progra, clases y ejercicios/Clase 10/edades.py
+++
@@ -1,8 +0,0 @@
-edades = []
-i = 0
-rep = ""
-print("Bienvenido al programa de edades")
-while rep != "*":
-    edades.append(int(input("Ingrese la edad: ")))
-    rep = input("Si desea salir ingrese *: ")
-print("Las edades ingresadas son: ", edades)
